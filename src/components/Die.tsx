8abcf7a94018a80117192a51dfce424636fbb0e3
Purice Victor <pvictor0110@gmail.com>
2023-11-28 13:42:31 +0200

    Update Die.tsx

--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import dice1 from "../assets/dice-1.png";
+import dice2 from "../assets/dice-2.png";
+import dice3 from "../assets/dice-3.png";
+import dice4 from "../assets/dice-4.png";
+import dice5 from "../assets/dice-5.png";
+import dice6 from "../assets/dice-6.png";
+
 
 interface DieProps {
   held: boolean;
@@ -10,22 +17,22 @@ export default function Die(props: DieProps): JSX.Element {
   let dieFace = "";
   switch (props.value) {
     case 1:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice1;
       break;
     case 2:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice2;
       break;
     case 3:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice3;
       break;
     case 4:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice4;
       break;
     case 5:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice5;
       break;
     case 6:
-      dieFace = `./src/assets/dice-${props.value}.png`;
+      dieFace = dice6;
       break;
     default:
       break;
